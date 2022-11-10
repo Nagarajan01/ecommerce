477d94bef67fb1f77a13b900c4ea102adf6de8d1
Nagarajan01 <nagarajansm01@gmail.com>
2022-11-10 10:20:31 +0530

    none

--- product_app/urls.py
+++ product_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
-#from product_app.views import CreateCheckoutSessionView, CancelView, SuccessView, ProductLandingPageView, stripe_webhook
 
 app_name = 'product_app'
 
@@ -9,7 +8,6 @@ urlpatterns = [
     path('listjson/', views.listjson.as_view(), name="listjson"),
     path('register/', views.registerPage, name="register"),
     path('view_cart/', views.view_cart.as_view(), name="view_cart"),
-
     path('view_cart_json/', views.view_cart_json.as_view(), name="view_cart_json"),
     path('accounts/login/', views.loginPage, name="login"),
     path('lists/', login_required(views.Lists.as_view()), name="Lists"),
@@ -31,45 +29,3 @@ urlpatterns = [
     path('webhook', views.stripe_webhook, name='stripe_webhook'),
 
 ]
-
-
-#path('search/', views.index.as_view(), name=   "search"),
-#
-# view_cart/orders/{{i.id}}
-#path('search_results/', views.SearchResultsView.as_view(), name="SearchResultsView"),
-
-
-'''
-    path('thanks/', views.thanks, name='thanks'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('stripe_webhook/', views.stripe_webhook, name='stripe_webhook')'''
-
-'''
-
-      <script src="https://js.stripe.com/v3/"></script>
-
-      <script>
-
-        const buy_now_button = document.querySelector('#buy_now_btn')
-
-        buy_now_button.addEventListener('click', event => {   
-          fetch('/checkout/')
-          .then((result) => { return result.json() })
-          .then((data) => {
-            var stripe = Stripe(data.stripe_public_key);
-
-            stripe.redirectToCheckout({
-            // Make the id field from the Checkout Session creation API response
-            // available to this file, so you can provide it as parameter here
-            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-              sessionId: data.session_id
-            }).then(function (result) {
-              // If `redirectToCheckout` fails due to a browser or network
-              // error, display the localized error message to your customer
-              // using `result.error.message`.
-            });
-          }) 
-        })
-      </script>''''''
-    path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
-    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),'''
--- product_app/views.py
+++ product_app/views.py
@@ -54,7 +54,6 @@ def checkout(request):
     )
     return redirect(session.url)
 
-global mydict1
 mylist1 = []
 mylist2 = []
 mylist3 = []
@@ -271,14 +270,15 @@ def order_view(request):
     context = {'cart_list': cart_list, 'my_total': price, 'tax': new_tax, 'total_price':int(total_price), 'order':order_list}
     return render(request, template, context)
     
+# >Below code is json convertions,
 '''
-            data = json.dumps(context, default=str, indent=1)
-            print(type(data))
-            return HttpResponse(data, content_type='application/json')'''
+data = json.dumps(context, default=str, indent=1)
+print(type(data))
+return HttpResponse(data, content_type='application/json')'''
 '''
-    data = json.dumps(context, default=str, indent=1)
-    print(type(data))
-    return HttpResponse(data, content_type='application/json')'''
+data = json.dumps(context, default=str, indent=1)
+print(type(data))
+return HttpResponse(data, content_type='application/json')'''
 
 
 @login_required(login_url='accounts/login/')
@@ -308,10 +308,12 @@ def get_queryset(request):
             my_list.append(index['wished_item_id'])
         context = {'object_list': form, 'my_list': my_list}
         return render(request, template_name, context)
+
+# >Below code is json convertions,
 '''
-        data = json.dumps(context, default=str, indent=1)
-        print(type(data))
-        return HttpResponse(data, content_type="application/json")'''
+data = json.dumps(context, default=str, indent=1)
+print(type(data))
+return HttpResponse(data, content_type="application/json")'''
 
 
 @login_required(login_url='accounts/login/')
@@ -353,140 +355,3 @@ class view_wishlist(ListView):
             mylist.append(ob_p[0])
         return mylist
 
-
-"""
-stripe.api_key = settings.STRIPE_PRIVATE_KEY
-
-class SuccessView(TemplateView):
-    template_name = "success.html"
-
-class CancelView(TemplateView):
-    template_name = "success.html"
-
-class ProductLandingPageView(TemplateView):
-    template_name = "accounts/landing.html"
-
-    def get_context_data(self, **kwargs):
-
-        product = Order.objects.all().values().last()
-        context = super(ProductLandingPageView, self).get_context_data(**kwargs)
-        context.update({
-            "product": product,
-            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
-        })
-        return context
-
-
-class CreateCheckoutSessionView(View):
-    def post(self, request, *args, **kwargs):
-        product_id = self.kwargs["pk"]
-        YOUR_DOMAIN = "http://127.0.0.1:8000"
-        
-        order = Order.objects.get(id=product_id)
-        checkout_session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[
-                {
-                    'price_data': {
-                        'description':"payment", 
-                        'currency': 'inr',
-                        'unit_amount': order.total,
-                        'product_data': {
-                            'name': order.user_id,
-                        },
-                    },
-                    'quantity': 1,
-                },
-            ],
-            metadata={
-                "product_id": order.id
-            },
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success/',
-            cancel_url=YOUR_DOMAIN + '/cancel/',
-        )
-        '''return JsonResponse({
-            'id': checkout_session.id
-        })'''
-        return redirect(checkout_session.url)
-
-
-
-@csrf_exempt
-def stripe_webhook(request):
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    event = None
-
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-        )
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
-
-    # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-
-        customer_email = session["customer_details"]["email"]
-        product_id = session["metadata"]["product_id"]
-
-        product = Product.objects.get(id=product_id)
-        """"""
-        send_mail(
-            subject="Here is your product",
-            message=f"Thanks for your purchase. Here is the product you ordered. The URL is {product.url}",
-            recipient_list=[customer_email],
-            from_email="matt@test.com"
-        )
-
-        # TODO - decide whether you want to send the file or the URL
-    
-    elif event["type"] == "payment_intent.succeeded":
-        intent = event['data']['object']
-
-        stripe_customer_id = intent["customer"]
-        stripe_customer = stripe.Customer.retrieve(stripe_customer_id)
-
-        customer_email = stripe_customer['email']
-        product_id = intent["metadata"]["product_id"]
-
-        product = Product.objects.get(id=product_id)
-
-        send_mail(
-            subject="Here is your product",
-            message=f"Thanks for your purchase. Here is the product you ordered. The URL is {product.url}",
-            recipient_list=[customer_email],
-            from_email="matt@test.com"
-        )
-
-    return HttpResponse(status=200)
-
-
-class StripeIntentView(View):
-    def post(self, request, *args, **kwargs):
-        try:
-            req_json = json.loads(request.body)
-            customer = stripe.Customer.create(email=req_json['email'])
-            product_id = self.kwargs["pk"]
-            product = Order.objects.get(id=product_id)
-            intent = stripe.PaymentIntent.create(
-                description = product.items.title,
-                amount=product.total,
-                currency='inr',
-                customer=customer['id'],
-                metadata={
-                    "product_id": product.id
-                }
-            )
-            return JsonResponse({
-                'clientSecret': intent['client_secret']
-            })
-        except Exception as e:
-            return JsonResponse({ 'error': str(e) })
-        """
